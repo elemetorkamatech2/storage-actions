35e2273cb296c5e2f79e80e5855bbfe1508a21c2
rivkabenhlomo <r548468129@Gmail.com>
2023-06-01 00:24:23 +0300

    Commit message describing changes made

--- test/test.js
+++ test/test.js
@@ -1,14 +1,46 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const chai = require('chai');
+const expect = chai.expect;
 const assert = require('assert');
-describe('MongoDB Connection', function () {
-    it('should connect to the database', function (done) {
-        this.timeout(10000);
-      MongoClient.connect('mongodb://localhost:27017/Communicion', function (err, db) {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
-        db.close();
-        done();
+const { describe, it } = require('mocha');
+describe('MongoDB Connection', function() {
+  it('should connect to MongoDB', function() {
+    return mongoose.connect('mongodb://localhost:27017/storageAction')
+      .then(() => {
+        console.log('MongoDB connection successful');
+      })
+      .catch((err) => {
+        console.log('MongoDB connection error:', err);
+        throw err;
       });
-    });
   });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const request = require('supertest');
+// const app = require('./app');
+
+// describe('Database connection', () => {
+//   it('should connect to MongoDB', (done) => {
+//     request(app)
+//       .get('/test-db-connection')
+//       .expect(200)
+//       .end((err, res) => {
+//         if (err) return done(err);
+//         done();
+//       });
+//   });
+// })
+
   //mocha --recursive test
