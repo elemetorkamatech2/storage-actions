89d04719b4ebb5e598e6eadaef38b369a78f80ce
RACHELIgit <128489099+RACHELIgit@users.noreply.github.com>
2023-06-15 17:19:36 +0300

    Update backupRoute.js

---
+++ api/routes/backupRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+const {
+    create,
+} = require('../controller/backupControler')
+
+router.post('/:id', create)
+module.exports = router
