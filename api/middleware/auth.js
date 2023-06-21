ef833ecc82e7aac2c43a6cacf88f82589bddfb1c
rivkabenhlomo <r548468129@Gmail.com>
2023-06-21 10:09:40 +0300

    the chacke token5

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -5,11 +5,12 @@ const logger = require('../../logger');
 dotenv.config();
 module.exports = (req, res, next) => {
   try {
+    const useridbody = req.body.userId;
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.PUBLIC_KEY);
     const { userId } = decodedToken;
     logger.info(userId);
-    if (req.body.userId && req.body.userId !== userId) {
+    if (useridbody && useridbody !== userId) {
       throw new Error('Invalid user ID');
     } else {
       next();
