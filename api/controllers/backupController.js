5fcbc3afc26abd53c6ba92da2f9ece3d67bd9636
RACHELIgit <128489099+RACHELIgit@users.noreply.github.com>
2023-06-25 16:02:29 +0300

    update backupController

--- api/controllers/backupController.js
+++ api/controllers/backupController.js
@@ -1,26 +1,14 @@
-const Website = require('../models/websiteModel');
 const { findById } = require('../models/websiteModel');
-const Backup = require('../models/backupModel');
 
 module.exports = {
   createBackup: async (req, res) => {
     try {
       const websiteToBackup = await findById(req.params.id).exec();
       if (!websiteToBackup) return res.status(404).send('Website not found!');
-      if (websiteToBackup.backup) return res.status(404).send('This website is already backed up');
-      const backupWebsiteData = {
-        ...websiteToBackup.toObject(),
-        status: 'backup',
-        _id: undefined,
-      };
-     await new Website(backupWebsiteData).save();
-      const backup = await new Backup({
-        siteId: websiteToBackup.id,
-        description: websiteToBackup.description,
-      }).save();
-      websiteToBackup.backup = backup.id;
+      if (websiteToBackup.backups.length > 0) return res.status(404).send('This website is already backed up');
+      websiteToBackup.ImportantMessages = 'In the process of creating a backup'
       await websiteToBackup.save();
-      return res.status(200).send('Backup created successfully');
+      return res.status(200).send('update successfully');
     } catch (err) {
       return res.status(500).send({ error: err.message });
     }
